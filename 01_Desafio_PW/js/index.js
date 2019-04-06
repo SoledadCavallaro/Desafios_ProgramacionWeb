3c37cc87fb1aea5ac02b43a7bdcf72a0a251f6b5
Soledad <cavallaro.pellon@gmail.com>
2019-04-06 16:30:02 -0300

    desafios

---
+++ 01_Desafio_PW/js/index.js
@@ -0,0 +1,45 @@
+/* CALCULADORA CON IF ELSE
+
+var num1 = parseInt(prompt());
+var sign = prompt();
+var num2 = parseInt(prompt());
+var result;
+
+if (sign === '+') {
+    result = num1 + num2;
+} else if (sign === '-') {
+    result = num1 - num2;
+} else if (sign === '*') {
+    result = num1 * num2;
+} else if (sign === '/') {
+    result = num1 / num2;
+} else {
+    alert("no pusiste ningun signo valido")
+}
+console.log(result);
+ */
+
+/* SWITCH EJERCICIO CALCULADORA*/
+
+
+/* var num1 = parseInt(prompt());
+var sign = prompt();
+var num2 = parseInt(prompt());
+
+switch (sign) {
+    case '+':
+        console.log(num1 + num2);
+        break;
+    case '-':
+        console.log(num1 - num2);
+        break;
+    case '*':
+        console.log(num1 * num2);
+        break;
+    case '/':
+        console.log(num1 / num2);
+        break;
+    default:
+        alert('no pusiste nungun signo valido')
+} */
+
